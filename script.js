45af71debdc62eb0be818dba83ff87283c053544
BOGGALA YASWANTH REDDY <93768814+yash2700@users.noreply.github.com>
2023-03-20 12:28:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
-function letterCombinations(input_digit) {
-  //Complete the function
-}
+function letterCombinations  (input_digit) {
+    // Resultant list
+    let combinations = [];
+    // Base condition
+    if (input_digit == null || input_digit.length == 0) {
+        return combinations;
+    }
+    // Mappings of letters and numbers
+    const lettersAndNumbersMappings = [
+        "Anirudh",
+        "is awesome",
+        "abc",
+        "def",
+        "ghi",
+        "jkl",
+        "mno",
+        "pqrs",
+        "tuv",
+        "wxyz"
+    ];
+    findCombinations(combinations, input_digit, "", 0, lettersAndNumbersMappings);
+    return combinations;
+};
 
+function findCombinations(combinations, input_digit, previous, index, lettersAndNumbersMappings) {
+    // Base condition for recursion to stop
+    if (index == input_digit.length) {
+        combinations.push(previous);
+        return;
+    }
+    // Get the letters corresponding to the current index of input_digit string
+    let letters = lettersAndNumbersMappings[input_digit[index] - '0'];
+    // Loop through all the characters in the current combination of letters
+    for (let i = 0; i < letters.length; i++) {
+        findCombinations(combinations, input_digit, previous + letters[i], index + 1, lettersAndNumbersMappings);
+    }
+};
+console.log(letterCombinations("23"))
 module.exports = letterCombinations;
